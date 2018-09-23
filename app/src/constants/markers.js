1a87516233621c8429241c3babf7c0815e455051
Tobias Lundgren <tobias@lundgrendesign.se>
2018-09-24 01:20:10 +0200

    break out markers to constants

---
+++ app/src/constants/markers.js
@@ -0,0 +1,25 @@
+export const southernSwedenRegion = {
+  latitude: 59.3,
+  longitude: 14.9675,
+  latitudeDelta: 8.14,
+  longitudeDelta: 0
+};
+
+export const defaultMarkers = [
+  {
+    title: 'Stockholm',
+    description: 'Swedens biggest city',
+    coordinate: {
+      latitude: 59.36,
+      longitude: 18.26
+    }
+  },
+  {
+    title: 'Gothenburg',
+    description: 'Swedens best city',
+    coordinate: {
+      latitude: 57.71,
+      longitude: 11.98
+    }
+  }
+];
