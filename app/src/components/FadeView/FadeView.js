b09003996dc5eef250787832eccbe8963a9beb9d
Tobias Lundgren <tobias@lundgren.design>
2018-11-09 19:23:09 +0100

    Add FadeView component

---
+++ app/src/components/FadeView/FadeView.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Animated } from 'react-native';
+
+class FadeView extends Component {
+  state = {
+    fadeAnim: new Animated.Value(0),
+  };
+
+  componentWillReceiveProps(props) {
+    const fadeDuration = 1000;
+    if (!props.isVisible && this.props.isVisible) {
+      this.fadeOut(fadeDuration);
+    }
+  }
+
+  componentWillMount() {
+    const duration = 1000;
+    Animated.timing(this.state.fadeAnim, {
+      toValue: 1,
+      duration: duration,
+    }).start();
+  }
+
+  fadeOut = duration => {
+    Animated.timing(this.state.fadeAnim, {
+      toValue: 0,
+      duration: duration,
+    }).start(() => {
+      this.props.finished();
+    });
+  };
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View style={{ ...this.props.style, opacity: fadeAnim }}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+export default FadeView;
