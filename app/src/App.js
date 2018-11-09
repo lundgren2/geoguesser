93143c35dea81c70ca931eb82b192595746f4783
Tobias Lundgren <tobias@lundgren.design>
2018-11-09 19:36:51 +0100

    Remove WelcomeView

--- app/src/App.js
+++ app/src/App.js
@@ -4,14 +4,13 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import GameScreen from './screens/Game';
-import WelcomeView from './screens/Welcome';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default () => {
   return (
     <Provider store={store}>
-      <WelcomeView />
+      <GameScreen />
     </Provider>
   );
 };
