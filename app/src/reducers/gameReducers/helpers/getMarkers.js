ba9eef2a64803750896f57d09fd09339874fdee2
Jani Jokinen <janjo273@student.liu.se>
2018-11-14 15:07:57 +0100

    Added possibility of 5 different levels for getMarkers, with 5 different json files

--- app/src/reducers/gameReducers/helpers/getMarkers.js
+++ app/src/reducers/gameReducers/helpers/getMarkers.js
@@ -3,23 +3,33 @@ import _ from 'lodash';
 // Write amount of markers to be shown
 const MAX_NR_MARKERS = 6; //TODO: move this to a constant file
 
-const getMarkers = (level, shuffle = false) => {
+const getMarkers = (level) => {
   let listCities;
   switch (level) {
     case 1:
-      listCities = require('../../../constants/levelsJson/linkopingArea.json')
+      listCities = require('../../../constants/levelsJson/se.json')
         .geonames;
       break;
     case 2:
-      listCities = require('../../../constants/levelsJson/ostArea.json')
+      listCities = require('../../../constants/levelsJson/fi.json')
+        .geonames;
+      break;
+    case 3:
+      listCities = require('../../../constants/levelsJson/dk.json')
+        .geonames;
+      break;
+    case 4:
+      listCities = require('../../../constants/levelsJson/no.json')
+        .geonames;
+      break;
+    case 5:
+      listCities = require('../../../constants/levelsJson/de.json')
         .geonames;
       break;
     default:
-      listCities = require('../../../constants/levelsJson/sverigeCities.json')
+      listCities = require('../../../constants/levelsJson/se.json')
         .geonames;
   }
-
-  listCities = shuffle ? _.shuffle(listCities) : listCities;
 
   const NR_MARKERS =
     listCities.length >= MAX_NR_MARKERS ? MAX_NR_MARKERS : listCities.length;
@@ -27,7 +37,7 @@ const getMarkers = (level, shuffle = false) => {
   return listCities.slice(0, NR_MARKERS).map((city, index) => ({
     id: index,
     title: city.name,
-    description: city.fcodeName,
+    description: city.toponymName,
     coordinate: {
       latitude: parseFloat(city.lat),
       longitude: parseFloat(city.lng)
