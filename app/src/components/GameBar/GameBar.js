9e1d94d95aa157dfd36f0e7ad90c719c0ab6dc7f
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-20 10:21:28 +0100

    Move redux logic from Game.js to components

--- app/src/components/GameBar/GameBar.js
+++ app/src/components/GameBar/GameBar.js
@@ -60,7 +60,10 @@ class GameBar extends Component {
 
   render() {
     const { timer } = this.state;
-    const { correctMarker } = this.props;
+    const { correctMarker, showMainMenu } = this.props;
+
+    if (showMainMenu) return null;
+
     return (
       <View style={styles.bar}>
         <Filler progress={timer} />
@@ -73,6 +76,7 @@ class GameBar extends Component {
 const mapStateToProps = ({ game, layers }) => ({
   correctMarker: game.correctMarker,
   gameStatus: layers.gameStatus,
+  showMainMenu: layers.mainMenu,
 });
 
 export default connect(mapStateToProps)(GameBar);
