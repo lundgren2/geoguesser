77ba4ea75601b3d6854ab1b1e211577280e4b1e2
Tobias Lundgren <tobias@lundgren.design>
2018-12-10 18:20:13 +0100

    Style settings button

--- app/src/components/SettingsButton/styles.js
+++ app/src/components/SettingsButton/styles.js
@@ -3,9 +3,11 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     position: 'absolute',
-    top: 0,
-    right: 0,
+    top: 5,
+    right: 5,
     margin: 10,
   },
-  button: { backgroundColor: '#ECECEC', padding: 5 },
+  button: {
+    backgroundColor: 'transparent',
+  },
 });
