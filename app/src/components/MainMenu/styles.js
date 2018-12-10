24c90b53981403017382badaffab294bc33793d3
Tobias Lundgren <tobias@lundgren.design>
2018-12-10 18:50:22 +0100

    Main Menu style

--- app/src/components/MainMenu/styles.js
+++ app/src/components/MainMenu/styles.js
@@ -1,33 +1,34 @@
 import { StyleSheet } from 'react-native';
-import colors from 'constants/colors';
+import theme from 'constants/theme';
 
 export default StyleSheet.create({
   overlay: {
     flex: 1,
+    width: '100%',
     justifyContent: 'center',
     alignContent: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
     zIndex: 100,
-    padding: 40,
+    padding: 20,
   },
   buttonContainer: {
     marginBottom: 18,
   },
   button: {
     width: '100%',
-    borderRadius: 35,
-    padding: 12,
+    borderRadius: theme.buttonBorderRadius,
+    padding: 10,
   },
   buttonText: {
     fontWeight: 'bold',
   },
   logoContainer: {
-    height: 280,
+    height: 310,
     alignItems: 'center',
     marginBottom: 40,
   },
   logo: {
     maxHeight: '100%',
-    maxWidth: '90%',
+    maxWidth: '100%',
   },
 });
